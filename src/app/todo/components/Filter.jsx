0cbe5d4cfcc0fae86c46d8651bb38366267c885d
mrrobot2524 <alenjon2594@gmail.com>
2025-11-15 23:50:45 +0500

    task done and do some changes

--- src/app/todo/components/Filter.jsx
+++ src/app/todo/components/Filter.jsx
@@ -8,10 +8,10 @@ const Filter = ({value, onChange}) => {
             <Button variant="primary" sizeBtn="sm" className={`btn ${value === 'all' ? '': 'small'} w-full lg:w-auto`} onClick={() => onChange("all")}>
                 All
             </Button>
-            <Button variant="primary" sizeBtn="sm" className={`btn ${value === 'active' ? '': 'small'} w-full lg:w-auto`} onClick={() => onChange("active")}>
+            <Button variant="succsess" sizeBtn="sm" className={`btn ${value === 'active' ? '': 'small'} w-full lg:w-auto`} onClick={() => onChange("active")}>
                 Active
             </Button>
-            <Button variant="primary" sizeBtn="sm" className={`btn ${value === 'completed' ? '': 'small'} w-full lg:w-auto`} onClick={() => onChange("completed")}>
+            <Button variant="danger" sizeBtn="sm" className={`btn ${value === 'completed' ? '': 'small'} w-full lg:w-auto`} onClick={() => onChange("completed")}>
                 Completed
             </Button>
         </div>
