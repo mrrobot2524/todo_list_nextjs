3198863b75d24beb50065b8e43ef459297cdc638
mrrobot2524 <alenjon2594@gmail.com>
2025-11-15 22:30:25 +0500

    task done was some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
      <div>
           <h1>Добро пожаловать</h1>
-          <p>Перейти к  <Link href="/todo">Todo приложению</Link></p>
+          <p>Перейти к  <Link href="/todo" className="text-blue-300 underline">Todo приложению</Link></p>
       </div>
   );
 }
